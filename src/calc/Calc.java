d90673b5584c55073958922fea796244ea06faea
Dhruv Patel <43826893+dhruv5418@users.noreply.github.com>
2019-07-11 16:02:07 -0400

    Update Calc.java

--- src/calc/Calc.java
+++ src/calc/Calc.java
@@ -8,36 +8,52 @@ public class Calc {
 		// TODO Auto-generated method stub
 	Scanner i = new Scanner(System.in); 
 	System.out.println("Enter Operation");
-	char c = i.next().charAt(0); 	
+	char c = i.next().charAt(0); 
+	int d;
 	if(c=='+')
 	{
 		System.out.println("Enter two values");
 		int a=i.nextInt();
 		int b=i.nextInt();
+		d=a+b;
+		System.out.println("Sum="+d);
 	}
 	else if(c=='-')
 	{
 		System.out.println("Enter two values");
 		int a=i.nextInt();
 		int b=i.nextInt();
+		d=a-b;
+		System.out.println("Substraction="+d);
 	}
 	else if(c=='*') {
 		
 		System.out.println("Enter two values");
 		int a=i.nextInt();
 		int b=i.nextInt();
+		d=a*b;
+		System.out.println("Multiplication="+d);
 	}
 	else if(c=='/') {
 		
 		System.out.println("Enter two values");
 		int a=i.nextInt();
 		int b=i.nextInt();
+		d=a/b;
+		System.out.println("Division="+d);
 	}
 	else if(c=='%') {
 		
 		System.out.println("Enter two values");
 		int a=i.nextInt();
 		int b=i.nextInt();
+		d=a%b;
+		System.out.println("Modulo="+d);
+	}
+	else if(c=='!')
+	{
+		System.out.println("Enter two values");
+		int a=i.nextInt();
 	}
 	
 
