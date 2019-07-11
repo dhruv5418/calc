3904665d74fbf5c917f663ebfbe1f680e3205c18
1992744 <1992744@E20732.CEGEPGIM.LCL>
2019-07-11 15:16:22 -0400

    first commit

---
+++ src/calc/Calc.java
@@ -0,0 +1,28 @@
+package calc;
+
+import java.util.Scanner;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner i = new Scanner(System.in); 
+		
+		int a=i.nextInt();
+		int b=i.nextInt();
+		System.out.println("Enter Operater");
+		String g =i.nextLine();
+		int c;
+		switch(g) {
+		case "+":	c=a+b;
+				System.out.println("Addition=" +c); 
+				break;
+		case 20:System.out.println("20"); 
+				break;
+		case 30:System.out.println("30"); 
+				break;		
+		default:System.out.println("Invalid value entered"); 
+				break;		
+	}
+
+}
